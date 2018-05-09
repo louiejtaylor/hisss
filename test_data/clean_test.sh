5c3ede9d693fc1dbcd4bf28f220ac274831769b5
louiejtaylor <louiejtaylor@gmail.com>
2018-05-09 09:22:02 -0400

    add cleanup script for test data

---
+++ test_data/clean_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# script to clean up test output
+# for safety, will only run when called from a dir named "test_data"
+
+wd=`pwd`
+cur_dir=${wd##/*/}
+
+if [ "$cur_dir" != "test_data" ]; then
+	echo "current dir is '${cur_dir}' not 'test_data', exiting"
+	exit 1
+fi
+echo "in '${cur_dir}', attempting to remove indices and output directory"
+
+rm -r test_out
+rm genomes/pcv.fasta.*
+
+echo "done"
